73a34b48c0dadb43a6652ea9507e94f52de08e35
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-11-20 16:51:31 +0200

    update

--- main.go
+++ main.go
@@ -4,14 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func getCommand() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("pokedex >")
-	command, _ := reader.ReadString('\n')
-	fmt.Printf("The command %s unavailable", command)
+	for {
+		fmt.Print("pokedex >")
+		command, _ := reader.ReadString('\n')
+		command = strings.TrimSpace(command)
+
+		if command == "help" {
+			fmt.Printf("The %s is on the way\n", command)
+		}
+		if command == "exit" {
+			fmt.Printf("There is no %s\n", command)
+		}
+	}
+
 }
 
 func main() {
